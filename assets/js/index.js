2919540b0aace9111fd703c8f2947554c54ecd42
Sara mehlis <Sara.mehlis@gmail.com>
2024-01-28 13:25:06 +0000

    Add fight function

--- assets/js/index.js
+++ assets/js/index.js
@@ -48,3 +48,21 @@ let villainAttacks = [
     },
   };
   
+  function fight(hero, villain) {
+    //loop
+    while (hero.isAlive && villain.isAlive) {
+      let damageToVillain = hero.attack();
+      villain.health -= damageToVillain;
+  
+      let damageToHero = villain.attack();
+      hero.health -= damageToHero;
+  
+      if (hero.health <= 0) {
+        hero.die();
+      }
+      if (villain.health <= 0) {
+        villain.die();
+      }
+    }
+  }
+  fight(hero, villain);
